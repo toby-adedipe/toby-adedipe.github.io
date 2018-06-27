3ede9b29ead0cffc14eae0354f5d9e4221f20da3
toby-adedipe <toby_adedipe@yahoo.com>
2018-06-27 23:57:34 +0100

    registring service worker

--- js/sw-registration.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('service-worker.js')
-        .then((registration)=>{
-            console.log('Registered:', registration);
-        })
-        .catch(function(error){
-            console.log('Reigstration Failed: ', error);
-        });
-    }
