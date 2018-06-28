c604f4b362abd1b6d6e7c0717958ef81f520c0d3
toby-adedipe <toby_adedipe@yahoo.com>
2018-06-28 14:00:54 +0100

    testing service worker caching

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,3 +1,47 @@
-self.addEventListener('fetch', (event)=>{
+//self.addEventListener('fetch', (event)=>{
+//
+//})
 
+const CACHE_NAME = 'static-cache';
+const urlsToCache = [
+    '.',
+    'index.html',
+    'main.css',
+    'js/main.js'
+];
+
+self.addEventListener('install', (event)=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache)=>{
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+self.addEventListener(fetch, ()=>{
+    event.respondWith(
+        caches.match(event.request)
+        .then(()=>{
+            return response || fetchAndCache(event.request);
+        })
+    )
 })
+
+function fetchAndCache(url){
+    return fetch(url)
+        .then((response)=>{
+            //check for valid response
+            if(!response.ok){
+                throw Error(response.statusText);
+            }
+            return caches.open(CACHE_NAME)
+                .then((cache)=>{
+                    cache.put(url, response.clone());
+                    return response;
+                });
+        })
+        .catch((error)=>{
+            console.log('Request failed:', error);
+        });
+}
